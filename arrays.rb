a468deaaef1a6cacf28df2348f589b201eeed4e6
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-02-22 15:20:57 -0700

    Add variables for strings, integers, floats, booleans

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+teams = ["red sox", "liverpool", "avs","rockies", "nuggets"]
+years = [1989, 2000, 2008, 2020, 2022]
+payroll = [1000.01, 2000.02, 3000.03, 4000.04, 5000.05]
+champions = [true, true, true, false, false, false]
