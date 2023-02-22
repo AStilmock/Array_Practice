bfc3854b6a2ff02771933a511041e23013a7f37e
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-02-22 15:00:25 -0700

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+irb
+teams = ["red sox", "Liverpool", "avs", "nuggets"]
+years = [1989, 2000, 2020, 2022]
+payroll = [1958.2, 5000.00, 2030.5, 1500.50]
+champion = [true, true, true,false, false, false]
