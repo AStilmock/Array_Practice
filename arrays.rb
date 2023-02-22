3ee0f5f3d611dcc276b58cc2ee3921ec9815ab62
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-02-22 15:24:31 -0700

    Add called methods for arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,16 @@ teams = ["red sox", "liverpool", "avs","rockies", "nuggets"]
 years = [1989, 2000, 2008, 2020, 2022]
 payroll = [1000.01, 2000.02, 3000.03, 4000.04, 5000.05]
 champions = [true, true, true, false, false, false]
+
+# the .pop command removes the last element of a given array
+teams.pop
+
+# the .push command adds more elements to the array
+years.push[2024, 2025, 2026]
+
+# the .shift command removes the first element in an array and moves the array to include the remaining elements.
+payroll.shift
+
+# the .unshift command returns the array in its .shift form where elements have been shifted or removed
+champions.unshift
+
